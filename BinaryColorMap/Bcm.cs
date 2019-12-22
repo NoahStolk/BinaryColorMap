5e9a387dfa81a8f373d22ed6ddcd7cf536d1f496
Noah Stolk <noah.stolk1997@gmail.com>
2019-12-22 15:08:16 +0100

    Implement origin coordinates.

--- BinaryColorMap/Bcm.cs
+++ BinaryColorMap/Bcm.cs
@@ -6,20 +6,27 @@ namespace BinaryColorMap
 {
 	public class Bcm
 	{
+		private const int HeaderSize = 6;
+
 		public byte FrameCount { get; private set; }
 		public byte Width { get; private set; }
 		public byte Height { get; private set; }
+		public byte OriginX { get; private set; }
+		public byte OriginY { get; private set; }
 
 		public byte ColorCount => (byte)Palettes.Select(p => (int)p.ColorCount).Sum();
 
 		public byte[,,] PixelData { get; }
 		public List<Palette> Palettes { get; } = new List<Palette>();
 
-		public Bcm(byte frameCount, byte width, byte height)
+		public Bcm(byte frameCount, byte width, byte height, byte originX, byte originY)
 		{
 			FrameCount = frameCount;
 			Width = width;
 			Height = height;
+			OriginX = originX;
+			OriginY = originY;
+
 			PixelData = new byte[FrameCount, Width, Height];
 		}
 
@@ -30,29 +37,31 @@ namespace BinaryColorMap
 
 		public byte[] GetPixelData()
 		{
-			byte[] pixelData = new byte[4 + PixelData.Length];
+			byte[] pixelData = new byte[HeaderSize + PixelData.Length];
 
 			pixelData[0] = FrameCount;
-			pixelData[1] = Width;
-			pixelData[2] = Height;
-			pixelData[3] = ColorCount;
+			pixelData[1] = ColorCount;
+			pixelData[2] = Width;
+			pixelData[3] = Height;
+			pixelData[4] = OriginX;
+			pixelData[5] = OriginY;
 
 			for (int i = 0; i < FrameCount; i++)
 				for (int j = 0; j < Width; j++)
 					for (int k = 0; k < Height; k++)
-						pixelData[4 + i * Width * Height + j * Height + k] = PixelData[i, j, k];
+						pixelData[HeaderSize + i * Width * Height + j * Height + k] = PixelData[i, j, k];
 
 			return pixelData;
 		}
 
 		public static Bcm Create(byte[] pixelData)
 		{
-			Bcm bcm = new Bcm(pixelData[0], pixelData[1], pixelData[2]);
+			Bcm bcm = new Bcm(pixelData[0], pixelData[2], pixelData[3], pixelData[4], pixelData[5]);
 
 			for (int i = 0; i < bcm.FrameCount; i++)
 				for (int j = 0; j < bcm.Width; j++)
 					for (int k = 0; k < bcm.Height; k++)
-						bcm.PixelData[i, j, k] = pixelData[4 + i * bcm.Width * bcm.Height + j * bcm.Height + k];
+						bcm.PixelData[i, j, k] = pixelData[HeaderSize + i * bcm.Width * bcm.Height + j * bcm.Height + k];
 
 			return bcm;
 		}
